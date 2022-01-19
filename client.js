3966691362f666f002afdc2f4255bcb051d0d4a7
eze1er <papaezechiel@gmail.com>
2022-01-19 23:52:53 +0000

    input function and evenhundler

--- client.js
+++ client.js
@@ -10,29 +10,21 @@ const connect = function (socket) {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  conn.write("Name: EZE")
+  const myInterval = setInterval
+  // const up = 'Move: up';
+  // const right = 'Move: right';
+  // const down = 'Move: down';
+  // const left = 'Move: left';
+  // setInterval(() => { conn.write(`${up}`)}, 500);
+  // setInterval(() => { conn.write(`${right}`)}, 500);
+  // setInterval(() => { conn.write(`${down}`)}, 500);
+  // setInterval(() => { conn.write(`${left}`)}, 500);
   conn.on('connect', () => {
     // code that does something
     console.log('Successfully connected to game serve');
-    let count = 0;
-    const myInterval = setInterval(() => {
-      conn.write("Name: EZE");
-      conn.write("Move: up");
-      conn.write("Move: left");
-      conn.write("Move: down");
-      conn.write("Move: right");
-      conn.write("Move: up");
-      conn.write("Move: left"); 
-      if (count === 1500) myStopFunction(myInterval  );
-      count++;
-      console.log(count);
-    }, 2);
 
-    function myStopFunction() {
-      clearInterval(myInterval);
-    }
   });
-
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,8 +1,38 @@
 const { CONNREFUSED } = require("dns");
 // const net = require("net");
 const connect = require('./client');
+const stdin = process.stdin;
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  connect();
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
 
+  return stdin;
+};
+
+const handleUserInput = function (input) {
+  // your code here
+  if (input === '\u0003') {
+    process.exit();
+  }
+  // } else if (input === 'w') {
+  //   connect.write("Move: up");
+  // } else if (input === 'a') {
+  //   connect.write("Move: left");
+  // } else if (input === 's') {
+  //   connect.write("Move: down")
+  // } else if (input === 'd') {
+  //   connect.write("Move: right");
+  // }
+};
 
 console.log(`Connecting at port: `);
-connect();
+// connect();
+const stdin1 = setupInput();
+// stdin1.on('data', data => {
+//   console.log(`You typed ${data.toString()}`);
+// });
