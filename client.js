06f42bce49b40a6d8936b2e3966d6d4365f32859
eze1er <papaezechiel@gmail.com>
2022-01-19 20:35:51 +0000

    first commit for client.js and module.export

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function (socket) {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: 50541, // PORT number here,
+
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    // code that does something
+    conn.write('Name: EZE');
+    console.log('Successfully connected');
+
+  });
+  
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,26 +1,8 @@
 const { CONNREFUSED } = require("dns");
-const net = require("net");
+// const net = require("net");
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function (socket) {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541, // PORT number here,
 
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    // code that does something
-    conn.write('Name: EZE');
-    console.log('Successfully connected');
-
-  });
-  
-  return conn;
-};
 
 console.log(`Connecting at port: `);
 connect();
