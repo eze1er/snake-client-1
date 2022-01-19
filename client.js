7edafd4ab245d3e7f4b9d35e0bd15f90f6d2349c
eze1er <papaezechiel@gmail.com>
2022-01-19 21:58:18 +0000

    Snake move good

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { command } = require("yargs");
 // establishes a connection with the game server
 const connect = function (socket) {
   const conn = net.createConnection({
@@ -13,9 +14,25 @@ const connect = function (socket) {
   conn.on('connect', () => {
     // code that does something
     console.log('Successfully connected to game serve');
-    conn.write("Name: EZE");
+    let count = 0;
+    const myInterval = setInterval(() => {
+      conn.write("Name: EZE");
+      conn.write("Move: up");
+      conn.write("Move: left");
+      conn.write("Move: down");
+      conn.write("Move: right");
+      conn.write("Move: up");
+      conn.write("Move: left"); 
+      if (count === 1500) myStopFunction(myInterval  );
+      count++;
+      console.log(count);
+    }, 2);
+
+    function myStopFunction() {
+      clearInterval(myInterval);
+    }
   });
-  
+
   return conn;
 };
 
